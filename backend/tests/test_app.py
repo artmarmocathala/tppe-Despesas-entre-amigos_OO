5e73f1a58fa8ea0e1e1dc676ace99c62e2c83581
artmarmocathala <arthurmarmocathala@gmail.com>
2025-05-17 01:28:10 -0300

    fix(test): ignore tests

--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -1,6 +1,14 @@
 import pytest
 from app import app
 
-@pytest.fixture
+@pytest.mark.skip(reason="TDD")
 def test_always_passes():
     assert True
+
+@pytest.mark.skip(reason="TDD")    
+def test_always_fails():
+    assert False
+    
+@pytest.mark.skip(reason="TDD")
+def test_always_passes_2():
+    assert 4 > 3
